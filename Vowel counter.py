6085712fa12b6e22fff95f5bdadfd0aed18726b5
Ivan Zhilnikov <feliksesae@gmail.com>
2024-02-27 14:37:53 +0300

    add CI

--- Vowel counter.py
+++ Vowel counter.py
@@ -25,7 +25,8 @@ def count_vowels(text: str) -> int:
     ("Hello", 2),
     ('AeIou', 5),
     ("Python is awesome", 6),
-    ("Try to solve this task", 5)
+    ("Try to solve this task", 5),
+    ("bcdfghjklmnpqrstvwxyz", 0)
 ])
 def test_count_vowels(text, expected):
     assert count_vowels(text) == expected, \
